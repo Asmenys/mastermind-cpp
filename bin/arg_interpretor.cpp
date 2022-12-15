a4fc4e4dc18fe96fe22e5dec9867ae9994c9a1b3
Asmenys <76791232+Asmenys@users.noreply.github.com>
2022-12-15 12:50:05 +0200

    boost test dir, some functions for arg interpretor

--- bin/arg_interpretor.cpp
+++ bin/arg_interpretor.cpp
@@ -1,15 +1,24 @@
 #include "../bin/arg_interpretor.h"
-#include <iostream>
+#include <vector>
+
+std::vector<std::vector<std::string>> Arg_Interpretor::commands = {};
+
 void Arg_Interpretor::interpret_args(int argc, char *argv[]) {
   for (int i = 1; i < argc; i++) {
-    call_comand(argv[i][0]);
+    start_engine(argv[i][0]);
   }
 }
 
-void Arg_Interpretor::call_comand(char command) {
+void Arg_Interpretor::start_engine(char command) {
   if (command == 'p' || command == 'P') {
     std::cout << "start the game\n";
-  } else if (command == 'h' || 'H') {
-    std::cout << "hello\n";
+  } else if (command == 'h' || command == 'H') {
+    help();
+  }
+}
+void Arg_Interpretor::help(){
+  for(int i = 0; i<command_count; i++)
+  {
+    std::cout<<command_options[i]<<" "<<command_definitions[i]<<std::endl;
   }
 }
--- bin/arg_interpretor.h
+++ bin/arg_interpretor.h
@@ -1,6 +1,11 @@
-class Arg_Interpretor{
-    public:
-        static void interpret_args(int argc, char *argv[]);
-    private:
-        static void call_comand(char argv);
+#include <iostream>
+#include <vector>
+class Arg_Interpretor {
+public:
+  static void interpret_args(int argc, char *argv[]);
+  static std::vector<std::vector<std::string>> commands;
+
+private:
+  static void start_engine(char argv);
+  static void help();
 };
