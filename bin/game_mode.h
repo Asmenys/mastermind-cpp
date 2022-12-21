5c2f8a8e4c9d0c84266c457e10837c7993d57469
Asmenys <76791232+Asmenys@users.noreply.github.com>
2022-12-21 23:51:47 +0000

    added an include guard

--- bin/game_mode.h
+++ bin/game_mode.h
@@ -1,7 +1,10 @@
 #include <vector>
+#ifndef GAMEMODE_H
+#define GAMEMODE_H
 class GameMode {
 public:
   static void print_guess_results(std::vector<int> guess_comparison_results);
   static bool win_condition_check(std::vector<int> guess_comparison_results);
   static void print_vector(std::vector<int> input_vector);
 };
+#endif
